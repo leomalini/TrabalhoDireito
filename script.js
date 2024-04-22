42a81af48bcd1b518bfab42c05893646d1beede0
Leonardo Malini Coradini <leomalini61@gmail.com>
2024-04-22 20:50:19 -0300

    bug fix

--- script.js
+++ script.js
@@ -45,14 +45,14 @@ window.addEventListener("scroll", () => {
 
 // botao enviar 
 
-const btnEnviar = document.querySelector('#btn-enviar')
-const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+// const btnEnviar = document.querySelector('#btn-enviar')
+// const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
 
-btnEnviar.addEventListener("click", () => {
-    btnEnviarLoader.style.display = "block";
-    btnEnviar.style.display = "none";
-})
+// btnEnviar.addEventListener("click", () => {
+//     btnEnviarLoader.style.display = "block";
+//     btnEnviar.style.display = "none";
+// })
 
-setTimeout(() =>{
-    document.querySelector('#alerta').style.display = 'none';
-}, 5000)
+// setTimeout(() =>{
+//     document.querySelector('#alerta').style.display = 'none';
+// }, 5000)
